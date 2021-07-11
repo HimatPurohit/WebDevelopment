d9bd9596b31d8a9f70d1f2805761966b30ba3b6d
Himatkumar Purohit <himatpurohit1607@gmail.com>
2021-07-12 04:20:27 +0530

    Updated todo with Mongoose ODM for MongoDB

--- node and server based projects/todo-list/app.js
+++ node and server based projects/todo-list/app.js
@@ -1,5 +1,7 @@
 const express=require("express");
-const date=require(__dirname+"/date.js");
+const mongoose=require("mongoose");
+const lodash=require("lodash");
+// const date=require(__dirname+"/date.js");
 
 
 const app=express();
@@ -7,28 +9,131 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(express.static("public"));
 
-var items=["Buy Food","Cook Food","Eat Food"];
-var workItems=[];
-
 app.set("view engine","ejs");
 
+//Mongoose Connection to MongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false});
+
+const itemsSchema=new mongoose.Schema({name:String});
+
+const Item=mongoose.model("Item",itemsSchema);
+
+// Default items to be added for the first time
+const item1=new Item({
+    name:"Buy Food."
+});
+const item2=new Item({
+    name:"Hit + button to add new task."
+});
+const item3=new Item({
+    name:"<-----select this box to delete the task."
+});
+
+const defaultItems=[item1,item2,item3];
+
+//Schema for UserList
+const userListSchema=new mongoose.Schema({
+    name: String,
+    items: [itemsSchema]
+});
+
+const UserListItem=mongoose.model("userList",userListSchema);
+
+
 app.get("/",function(req,res){
-    res.render("list",{listTitle:date.day(), newListItems:items});
-})
-.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work", newListItems:workItems});
-})
-.post("/",function(req,res){
-    var item=req.body.newItem;
-    if(item!=""){
-        if(req.body.addButton==="Work"){
-            workItems.push(item);
-            res.redirect("/work")
-        }else{
-            items.push(item);
+    //fetching the data from database
+    Item.find({},function(err,result){
+        if(result.length===0){
+            Item.insertMany(defaultItems,function(err){
+                if(err){console.log(err)}
+            });
             res.redirect("/");
+        }else{
+            res.render("list",{listTitle:"Today", newListItems:result}); 
+        }
+    });
+})
+.get("/:userList",function(req,res){
+    const userListName= lodash.capitalize(req.params.userList);
+    
+    UserListItem.findOne({name:userListName},function(err,result){
+        if(!err){
+        if(!result){
+                const list=new UserListItem({
+                    name:userListName,
+                    items: defaultItems
+                });
+                list.save();
+            res.redirect("/"+userListName);
+        }else{
+            res.render("list",{listTitle:userListName, newListItems:result.items}); 
         }
     }
+
+});
+})
+.post("/",function(req,res){
+    var newItem=req.body.newItem;
+    var listname=req.body.listName;
+    // if(item!=""){
+    //     if(req.body.addButton==="Work"){
+    //         workItems.push(item);
+    //         res.redirect("/work")
+    //     }else{
+    //         items.push(item);
+    //         res.redirect("/");
+    //     }
+    // }
+
+    // Uploading latest Added Item in Database
+    if(newItem!=""){
+        const item=new Item({
+            name:newItem
+        });
+        // Checks the list to be used
+        if(listname==="Today"){
+            item.save();
+            res.redirect("/");
+        }else{
+            UserListItem.findOne({name:listname},function(err,result){
+            result.items.push(item);
+            result.save();
+            res.redirect("/"+listname);
+        });
+        }
+    }
+})
+.post("/delete",function(req,res){
+    // Deleting checked Item from Database
+    const id=req.body.checkbox;
+    var listname=req.body.listName;
+    // Item.deleteOne({_id:id},function(err){
+        // if(err){
+        //     console.log(err);
+        // }else{
+        //     res.redirect("/");
+        // }
+    // });
+    // Checks the list to be used
+    if(listname==="Today"){
+        Item.findByIdAndRemove(id,function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect("/");
+            };
+        });
+    }else{
+        UserListItem.findOneAndUpdate({name:listname},
+            {$pull:{items: {_id:id}}},function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect("/"+listname);
+            };
+        });
+    }
+    
 })
 
 app.listen(3000,function(){
